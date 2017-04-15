1a3db82fed87a4c5d42b514be46acb18d6f8c8d1
John Gilling <suddensleep@gmail.com>
2017-04-15 06:20:33 -0400

    Step 79: When right arrow is pressed at the end of the line, the cursor is moved to the beginning of the next line (if it exists).

--- kilo.c
+++ kilo.c
@@ -308,6 +308,10 @@ void editorMoveCursor(int key) {
     if (row && E.cx < row->size) {
       E.cx++;
     }
+    else if (row && E.cx == row->size) {
+      E.cy++;
+      E.cx = 0;
+    }
     break;
   case ARROW_UP:
     if (E.cy != 0) {
